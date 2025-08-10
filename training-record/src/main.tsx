24c46a8f20f8a5357bb280df2b3dcc414546e89e
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-08-10 22:14:46 +0900

    feat: Cognitoを使ったログインを実装

--- training-record/src/main.tsx
+++ training-record/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {AuthProvider} from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
+  response_type: "code",
+  scope: "phone openid email",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
