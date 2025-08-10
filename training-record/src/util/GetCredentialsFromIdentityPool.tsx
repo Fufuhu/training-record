f0af53ab689484a13bb7b27855a2bd0a882df92c
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-08-10 23:49:11 +0900

    refactor: コードをTypeScript的に改善

--- training-record/src/util/GetCredentialsFromIdentityPool.tsx
+++ training-record/src/util/GetCredentialsFromIdentityPool.tsx
@@ -1,11 +1,19 @@
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
+import type { AwsCredentialIdentity } from "@aws-sdk/types";
 
-export function GetCredentialsFromIdentityPool(idToken: string) {
-  const credentials = fromCognitoIdentityPool({
+export async function getCredentialsFromIdentityPool(idToken: string): Promise<AwsCredentialIdentity> {
+  const identityPoolId = import.meta.env.VITE_COGNITO_IDENTITY_POOL_ID as string;
+  const userPoolId = import.meta.env.VITE_COGNITO_USER_POOL_ID as string;
+
+  if (!identityPoolId || !userPoolId) {
+    throw new Error("Cognito Identity Pool ID(VITE_COGNITO_IDENTITY_POOL_ID) or User Pool ID(VITE_COGNITO_USER_POOL_ID) is not set in environment variables.");
+  }
+
+  const credentials = await fromCognitoIdentityPool({
     clientConfig: { region: "ap-northeast-1" },
-    identityPoolId: import.meta.env.VITE_COGNITO_IDENTITY_POOL_ID,
+    identityPoolId: identityPoolId,
      logins: {
-       [`cognito-idp.ap-northeast-1.amazonaws.com/${import.meta.env.VITE_COGNITO_USER_POOL_ID}`]: idToken,
+       [`cognito-idp.ap-northeast-1.amazonaws.com/${userPoolId}`]: idToken,
      }
   });
   return credentials;
