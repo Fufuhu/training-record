d5aace8b51a13e25cd1b00b9cecc1b6e1de5a3d9
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-08-19 23:11:32 +0900

    feat: 画面上部のバーを表示するようにした

--- training-record/src/layout/RecordsLayout.tsx
+++ training-record/src/layout/RecordsLayout.tsx
@@ -1,42 +1,14 @@
 import {Outlet} from "react-router";
-import {Calendar} from "../component/Calendar.tsx";
-import {Grid} from "@mui/material";
-import Button from "@mui/material/Button";
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import {useState} from "react";
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import {TopBar} from "../component/TopBar.tsx";
 
 export const RecordsLayout = () => {
 
-  const [isOpen, setIsOpen] = useState(true);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  }
 
   return (
     <div>
-      <Grid container spacing={2}>
-        <Grid size={isOpen ? 2 : 0}>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => toggleMenu()}
-          >{isOpen ? <ArrowLeftIcon/> : <ArrowRightIcon/>}</Button>
-          <div style={{
-            display: isOpen ? 'block' : 'none',
-          }}>
-            <Calendar />
-          </div>
-        </Grid>
-        <Grid  size={10}>
-          <div style={{
-            marginTop: '2em',
-          }}>
-            <Outlet />
-          </div>
-        </Grid>
-      </Grid>
+      <TopBar/>
+      <Outlet />
     </div>
   )
 }
