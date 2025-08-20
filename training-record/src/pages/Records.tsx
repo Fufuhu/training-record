23eb33f1403955e12991899637fa37cd705af677
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-08-20 23:28:26 +0900

    feat: 記録用のテーブルを作成

--- training-record/src/pages/Records.tsx
+++ training-record/src/pages/Records.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 
 export const Records = () => {
   const { year, month, day } = useParams();
@@ -13,6 +14,32 @@ export const Records = () => {
         <div>月: {month}</div>
         <div>日: {day}</div>
       </div>
+
+
+      <Paper>
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }} aria-label="training records">
+            <TableHead>
+              <TableRow>
+                <TableCell>種目</TableCell>
+                <TableCell>重量</TableCell>
+                <TableCell>セット数</TableCell>
+                <TableCell>レップ数</TableCell>
+                <TableCell>総レップ数</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>ベンチプレス</TableCell>
+                <TableCell>60</TableCell>
+                <TableCell>3</TableCell>
+                <TableCell>15</TableCell>
+                <TableCell>45</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   );
 }
